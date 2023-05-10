00a94d7013dbd2848abba1139337b8090c52356e
dev-suren <surenrana113@gmail.com>
2023-05-10 13:30:02 +0530

    adding some of the controller and error page handling

--- src/main/java/com/loanmanagementsystem/loanmanagementsystem/controller/NormalUsers.java
+++ src/main/java/com/loanmanagementsystem/loanmanagementsystem/controller/NormalUsers.java
@@ -2,9 +2,7 @@ package com.loanmanagementsystem.loanmanagementsystem.controller;
 
 import com.loanmanagementsystem.loanmanagementsystem.config.UserInfoUserDetails;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
